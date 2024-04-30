5fe000feb045d928ff5902830b58f509734ffbd1
Tashila Pathum <tashila.pathum@gmail.com>
2024-04-30 10:10:47 +0530

    Refactor and cleanup

--- lib/utils.dart
+++ lib/utils.dart
@@ -24,6 +24,5 @@ String getCheckValue({
 }) {
   String hash1 = sha512.convert(utf8.encode(merchantToken)).toString().toUpperCase();
   String hash2 = sha512.convert(utf8.encode("$merchantKey|$invoiceId|$amount|$currencyCode|$hash1")).toString().toUpperCase();
-  log("CHECK_VALUE: $hash2");
   return hash2;
 }
