7d868f994819ba1e2721a16c79fc77dc16f14c70
Tashila Pathum <tashila.pathum@gmail.com>
2024-05-17 10:56:40 +0530

    Added envs

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,12 +1,27 @@
 enum IPGEnvironment {
-  production,
+  dev,
+  qa,
   sandbox,
+  live,
 }
 
 String getEndpoint(IPGEnvironment? environment) {
-  if (environment == IPGEnvironment.sandbox) {
-    return 'https://payable-ipg-dev.web.app/ipg/dev';
-  } else {
-    return 'https://payable-ipg-dev.web.app/ipg/dev'; //TODO: add live
+  switch (environment) {
+    case IPGEnvironment.dev:
+      "https://payable-ipg-dev.web.app/ipg/dev";
+      break;
+    case IPGEnvironment.qa:
+      "https://qaipgpayment.payable.lk";
+      break;
+    case IPGEnvironment.sandbox:
+      "https://sandboxipgpayment.payable.lk";
+      break;
+    case IPGEnvironment.live:
+      "https://ipgpayment.payable.lk";
+      break;
+    case null:
+      "https://payable-ipg-dev.web.app/ipg/dev";
+      break;
   }
+  return "https://payable-ipg-dev.web.app/ipg/dev";
 }
